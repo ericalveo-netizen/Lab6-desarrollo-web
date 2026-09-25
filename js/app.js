f3b73686a21c8a8ce6def2ee0c2712d7602670dc
ericalveo-netizen <ericingutierrez@gmail.com>
2026-09-24 20:23:45 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
-// ===== JAVASCRIPT DEL LABORATORIO 6 =====
-
-document.addEventListener("DOMContentLoaded", function () {
-    const formulario = document.querySelector("form");
-
-    if (formulario) {
-        formulario.addEventListener("submit", function (evento) {
-            evento.preventDefault();
-            alert("Solicitud de cita registrada correctamente.");
-        });
-    }
-
-    console.log("Clínica veterinaria - Laboratorio 6 cargado correctamente.");
-});
+{
+  "nombre": "Eric",
+  "apellido": "Alveo",
+  "cedula": "8-1032-304",
+  "codigo_sesion": "LAB6-V9S3"
+}
